acdc444f179b19c578a502e7766378f2193cd3dd
sandra248 <104040301+sandra248@users.noreply.github.com>
2022-05-01 18:38:53 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+let tiempoVuelta1: number = 18;
+let tiempoVuelta2: number = 20;
+let tiempoVuelta3: number= 17;
+let tiempoVuelta4: number = 22;
+let tiempoTotalDeVueltas: number = (tiempoVuelta1 + tiempoVuelta2 + tiempoVuelta3 + tiempoVuelta4);
+
+console.log("El tiempo Total de vueltas es de" : tiempoTotalDeVueltas "minutos");
+
+let promedioDeVuelta: number = (tiempoTotalDeVueltas/4);
+
+console.log("El promedio de vuelta es de" promedioDeVuelta "minutos");
+
+
